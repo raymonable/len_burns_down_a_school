c235b75f77e972e66c9cb4ba496d8e628a32689c
Raymond <101374892+raymonable@users.noreply.github.com>
2022-12-04 19:57:57 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -11,13 +11,14 @@ self.addEventListener('message', e => {
 
 self.addEventListener('fetch', function (event) {
     var url = event.request.url.split('?')[0];
-    //console.log('a')
+    console.log('a')
     if (!!url.split('.')[1] && !!event.request.url.split('?')[1]) {
-        //console.log('1');
+        console.log('1');
         if (url.split('.')[1] == 'ks') {
             var file = url.split('/')[url.split('/').length - 1];
             var updirectory = url.split('/')[url.split('/').length - 2];
             var directory = (updirectory == "system" ? "system/" : "")
+            console.log(file, updirectory, directory)
             if (censored) {
                 event.respondWith(fetch('censorship/'+directory+file));
             } else {
